ec7311e32373f3fc6e51201d8f51490ba73bb17a
st1601 <87642035+st1601@users.noreply.github.com>
2022-04-20 14:30:54 +0700

    son

--- app.js
+++ app.js
@@ -187,12 +187,13 @@ app.get('/addBook', async (req, res) => {
 
     // const categories = await getAllDocumentsFromCollection('Category');
     // console.log(categories)
-    // res.render('addBook', {categories: categories});
+    res.render('addBook',)
+    // {categories: categories}
 })
-// app.post('/addCategory', async (req, res) => {
-//     const name = req.body.txtName
-//     const description = req.body.txtDescription
-//     const collectionName = 'Category'
+app.post('/addCategory', async (req, res) => {
+    const name = req.body.txtName
+    const description = req.body.txtDescription
+    const collectionName = 'Category'
 
     const newP = { name: name, description: description }
 
